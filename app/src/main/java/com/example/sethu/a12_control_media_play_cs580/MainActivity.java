70cf2345cb8cc137c38ebf509341e8bd68fe8bc8
unknown <smadhav2@binghamton.edu>
2017-11-30 23:16:33 -0500

    Play/Pause media implemented

--- app/src/main/java/com/example/sethu/a12_control_media_play_cs580/MainActivity.java
+++ app/src/main/java/com/example/sethu/a12_control_media_play_cs580/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.sethu.a12_control_media_play_cs580;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 import android.hardware.Sensor;
@@ -13,9 +10,6 @@ import android.hardware.SensorEvent;
 import android.widget.Toast;
 import android.media.AudioManager;
 import android.content.Intent;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -36,18 +30,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }else{
             System.out.println("GOTTTTTTTTT THEEEEE SENSORRRRR");
         }
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -60,22 +42,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // More code goes here
         if(sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             if (sensorEvent.values[0] < sensor.getMaximumRange()) {
+                try{
                 // Detected something nearby
-    //            getWindow().getDecorView().setBackgroundColor(Color.RED);
-                Toast.makeText(getApplicationContext(), "NEAR", Toast.LENGTH_SHORT).show();
-                AudioManager mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
-
-                if (mAudioManager.isMusicActive()) {
-                    System.out.println("MUSIC PLAYING=========="+"\n");
+                    Toast.makeText(getApplicationContext(), "NEAR", Toast.LENGTH_SHORT).show();
+                    AudioManager mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+                    System.out.println("SENSOR IN CLOSE RANGE==========" + "\n");
                     Intent i = new Intent("com.android.music.musicservicecommand");
 
-                    i.putExtra("command", "pause");
+                    i.putExtra("command", "togglepause");
                     MainActivity.this.sendBroadcast(i);
+
+                }catch(Exception e){
+                    e.printStackTrace();
                 }
 
             } else {
                 // Nothing is nearby
-    //            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
                 Toast.makeText(getApplicationContext(), "FAR", Toast.LENGTH_SHORT).show();
             }
         }
